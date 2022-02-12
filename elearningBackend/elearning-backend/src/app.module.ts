cda0073235a061d57a35ef8ddda176762bd58d48
abdulrahman <mrabdulrahman95@gmail.com>
2022-02-12 13:10:13 +0300

    added frontend

--- elearningBackend/elearning-backend/src/app.module.ts
+++ elearningBackend/elearning-backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal:true,
+  }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port:Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: false,
+      synchronize: true,
+      ssl:process.env.ENVIRONMENT=="dev"?false:{rejectUnauthorized:false,},
+      // entities: [__dirname + '/../**/*.entity.{js,ts}']
+
+      entities:[]
+  }),
+    StudentModule,
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ elearningFrontEND/elearningfrontend/android/app/src/main/kotlin/com/example/elearningfrontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elearningfrontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
